ec9923d7dac1e3ec2bc0cc4c509d5f3caf6695ce
Robin De Paepe <robindepaepe2000@gmail.com>
2023-12-29 16:46:56 +0100

    Adding UI + its functionality (input fields for game settings - restarting game - display information - error messaging)

--- Assets/Scripts/MapBehaviour.cs
+++ Assets/Scripts/MapBehaviour.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 using Random = UnityEngine.Random;
@@ -12,6 +13,15 @@ public class MapBehaviour : MonoBehaviour
     [SerializeField] private int m_MapHeight;
     [SerializeField] private int m_MineCount;
 
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI m_MineCounterText;
+    [SerializeField] private TextMeshProUGUI m_TimerText;
+    [SerializeField] private TextMeshProUGUI m_ErrorMessage;
+
+    [SerializeField] private TextMeshProUGUI m_WidthInputField;
+    [SerializeField] private TextMeshProUGUI m_HeightInputField;
+    [SerializeField] private TextMeshProUGUI m_MineCountInputField;
+
     //regular variables
     private Tilemap m_Tilemap;
     private MapCell[,] m_Cells;
@@ -19,36 +29,44 @@ public class MapBehaviour : MonoBehaviour
 
     private bool m_FirstClick = true;
     private bool m_GameOver = false;
+
     private int m_TilesToRevealForWin;
+    private int m_MineCounter;
+    private float m_Timer = 0f;
+
+    private int m_UpdatedMapWidth;
+    private int m_UpdatedMapHeight;
+    private int m_UpdatedMineCount;
     #endregion
 
     #region LifeTime
     private void Awake()
     {
+        //setup variables
         m_Tilemap = GetComponentInChildren<Tilemap>();
-        m_TilesToRevealForWin = (m_MapWidth * m_MapHeight) - m_MineCount;
 
-        //Check if the game is valid to start
-        if (m_Tilemap == null)
-        {
-            Debug.LogError("The tilemap for the map is missing.");
-            return;
-        }
+        m_WidthInputField.text = m_MapWidth.ToString();
+        m_HeightInputField.text = m_MapHeight.ToString();
+        m_MineCountInputField.text = m_MineCount.ToString();
 
-        if (m_MineCount > ((m_MapWidth * m_MapHeight) - 9))//Our first click is a 0 tile which means at least 9 tiles can't be a mine
-        {
-            Debug.LogError("There are to many bombs for the size of the map");
-            //Todo - Add player feedback why game won't start
-            return;
-        }
-        //Create the map structure
-        GenerateMap();
+        m_UpdatedMapWidth = m_MapWidth;
+        m_UpdatedMapHeight = m_MapHeight;
+        m_UpdatedMineCount = m_MineCount;
+
+        //start the game
+        StartGame();
     }
 
     private void Update()
     {
         if (m_GameOver)
             return;
+
+        if (!m_FirstClick)
+        {
+            m_Timer += Time.deltaTime;
+            m_TimerText.text = m_Timer.ToString("F2");
+        }
 
         if (Input.GetMouseButtonDown(0))//left mouse click occured
             HandleLeftMouseClick();
@@ -58,7 +76,6 @@ public class MapBehaviour : MonoBehaviour
     #endregion
 
     #region Core Functionality
-
     private void HandleLeftMouseClick()
     {
         //Find the correct position
@@ -179,9 +196,17 @@ public class MapBehaviour : MonoBehaviour
 
         //Set or unset the flag depending on the state
         if (m_Cells[cellPos.x, cellPos.y].State == MapCellState.TileFlag)
+        {
             UpdateCellState(cellPos, MapCellState.TileUnknown);
+            ++m_MineCounter;
+            m_MineCounterText.text = m_MineCounter.ToString();
+        }
         else if (m_Cells[cellPos.x, cellPos.y].State == MapCellState.TileUnknown)
+        {
             UpdateCellState(cellPos, MapCellState.TileFlag);
+            --m_MineCounter;
+            m_MineCounterText.text = m_MineCounter.ToString();
+        }
     }
 
     //Reveal all tiles that are allowed by game rules
@@ -212,6 +237,8 @@ public class MapBehaviour : MonoBehaviour
     {
         //setup variables
         m_Cells = new MapCell[m_MapWidth, m_MapHeight];
+        m_Tilemap.ClearAllTiles();
+
         Tile unknownTile = Resources.Load<Tile>(m_TilesPath + MapCellState.TileUnknown.ToString() + "");
 
         //Loop over the map cells
@@ -255,9 +282,94 @@ public class MapBehaviour : MonoBehaviour
     #endregion
     #endregion
 
+    #region Public functions
+    public void StartGame()
+    {
+        //Check if the game is valid to start
+        if (m_Tilemap == null)
+        {
+            LogError("The tilemap for the map is missing.");
+            return;
+        }
+
+        if (m_UpdatedMineCount > ((m_UpdatedMapWidth * m_UpdatedMapHeight) - 9))//Our first click is a 0 tile which means at least 9 tiles can't be a mine
+        {
+            LogError("There are to many bombs for the size of the map");
+            return;
+        }
+
+        //Setup variables correctly
+        m_MineCounter = m_MineCount = m_UpdatedMineCount;
+        m_MapHeight = m_UpdatedMapHeight;
+        m_MapWidth = m_UpdatedMapWidth;
+
+        m_TilesToRevealForWin = (m_MapWidth * m_MapHeight) - m_MineCount;
+        m_Timer = 0f;
+        m_FirstClick = true;
+        m_GameOver = false;
+
+        m_ErrorMessage.gameObject.SetActive(false);
+        m_MineCounterText.text = m_MineCounter.ToString();
+        m_TimerText.text = m_Timer.ToString("F2");
+
+        //Create the map structure 
+        GenerateMap();
+    }
+
+    public void UpdateGameWidth(string newValue)
+    {
+        int temp = CheckInputFieldDataForValidNumber(newValue);
+
+        if (temp > 0)
+            m_UpdatedMapWidth = temp;
+    }
+
+    public void UpdateGameHeight(string newValue)
+    {
+        int temp = CheckInputFieldDataForValidNumber(newValue);
+
+        if (temp > 0)
+            m_UpdatedMapHeight = temp;
+    }
+
+    public void UpdateMineCounter(string newValue)
+    {
+        int temp = CheckInputFieldDataForValidNumber(newValue);
+
+        if (temp > 0)
+            m_UpdatedMineCount = temp;
+    }
+    #endregion
+
     #region helperFunctions
+
+    private void LogError(string message)
+    {
+        m_ErrorMessage.text = message;
+        m_ErrorMessage.gameObject.SetActive(true);
+    }
+
+    private int CheckInputFieldDataForValidNumber(string value)
+    {
+        int newIntValue = -1;
+
+        int.TryParse(value, out newIntValue);
+
+        if (newIntValue <= 0)
+            LogError("Invalid input given, the data has to be a number larger than 0");
+        else
+            m_ErrorMessage.gameObject.SetActive(false);
+
+        return newIntValue;
+    }
+
     private void UpdateCellState(Vector3Int pos, MapCellState state)
     {
+        if (!IsValidPos(pos))
+        {
+            LogError("Something went wrong with the game");
+            return;
+        }
         m_Cells[pos.x, pos.y].State = state;
         m_Tilemap.SetTile(m_Cells[pos.x, pos.y].Position, Resources.Load<Tile>(m_TilesPath + state.ToString() + ""));
     }
